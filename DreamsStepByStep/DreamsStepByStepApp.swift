cb45270d230f0fd5ca79567e09f364461d749047
jamal <jamalzare514@gmail.com>
2021-04-04 16:23:46 +0430

    Initial Commit

---
+++ DreamsStepByStep/DreamsStepByStepApp.swift
@@ -0,0 +1,20 @@
+//
+//  DreamsStepByStepApp.swift
+//  DreamsStepByStep
+//
+//  Created by Jamal on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct DreamsStepByStepApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
